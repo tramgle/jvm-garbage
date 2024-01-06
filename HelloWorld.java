b33432cb54ecbfab1580176bf93962b4892deb9a
Sterling Tolley <sterling.tolley@strakertranslations.com>
2024-01-05 18:29:36 -0700

    More garbage

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,5 @@
+import stuff.Package;
+
 class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, world! I haven't done Java in a long time!");
---
+++ stuff/Package.java
@@ -0,0 +1,9 @@
+package stuff;
+
+public class Package {
+  final int size;
+
+  Package() {
+    size = 4;
+  }
+}
